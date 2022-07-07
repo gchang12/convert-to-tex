c85e07c8c522cc2625d140316e1082a412e0c802
George Chang <59218296+gchang12@users.noreply.github.com>
2022-07-07 10:08:21 -1000

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 my_src="sample";
 my_tgt="output";
+# Either of these programs will do
 my_cfile="c_process-files.out"; if [ ! -e $my_cfile ]; then gcc -o $my_cfile ${my_cfile/.out/.c}; fi; ./$my_cfile $my_src $my_tgt
 #./sh_process-files.sh $my_src $my_tgt
+cd output
+# Testing that the output is right.
+tex ./story.tex
